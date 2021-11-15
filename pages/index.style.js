ecea27a61788b2d96f2e48a286e28d47f8af6b79
Piouson <piouson@gmail.com>
2021-11-15 15:53:49 +0000

    Add emotion
    - add `@emotion/css`
    - add favicon
    - add `jsconfig` to specify `baseUrl, paths`
    - update `index.js`

---
+++ pages/index.style.js
@@ -0,0 +1,20 @@
+import { css } from '@emotion/css';
+
+export const container = css`
+  padding: 0 2rem;
+`;
+
+export const main = css`
+  min-height: 100vh;
+  padding: 4rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const title = css`
+  color: #0070f3;
+  text-decoration: none;
+`;

49154fc399421151f175ce4c21b0dc6ef7d36c4c
cyruskarsan <cyruskarsan@gmail.com>
2021-11-17 22:26:30 -0800

    reformat code

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,13 @@ import { MainApp } from '@src/components'
 import * as styles from '@src/components/mainApp/index.style'
 import { DAppProvider } from '@usedapp/core'
 
-export default function Home ()
-{
+export default function Home() {
   return (
     <DAppProvider config={{}}>
-          <MainApp>
-      <h1 className={ styles.title }>NFT Marketplace</h1>
-      <p>put a button to enter app here</p>
-    </MainApp>
+      <MainApp>
+        <h1 className={styles.title}>NFT Marketplace</h1>
+        <p>put a button to enter app here</p>
+      </MainApp>
     </DAppProvider>
 
   )

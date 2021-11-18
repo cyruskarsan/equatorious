b5cad66151eba122a79f0c299f741f9f42a417ba
DrewMcArthur <DrewMcArthur1@gmail.com>
2021-11-17 22:53:19 -0500

    add mint page and mint nft contract call

--- components/mintPage/MintPage.js
+++ components/mintPage/MintPage.js
@@ -1,6 +1,36 @@
-export default function MintPage ()
+import mintNft from "@src/contractCalls/mintNft"
+// import MintNftRequest from "@src/contractCalls/MintNftRequest"
+import * as React from "react"
+// import * as ReactDOM from "react-dom"
+
+const MintPage = () =>
 {
+  const mint = ( event ) =>
+  {
+    event.preventDefault()
+    // const requestBody: MintNftRequest = {
+    const requestBody = {
+      name: event.target.name.value,
+      file_url: event.target.url.value,
+      mint_to_address: event.target.addr.value
+    }
+    return mintNft( requestBody )
+  }
+
   return (
-    <div>MINT PAGE</div>
+    <form onSubmit={ mint } >
+      <label htmlFor="name" >Name</label>
+      <input id="name" type="text" />
+      <br />
+      <label htmlFor="url" >URL</label>
+      <input id="url" type="text" />
+      <br />
+      <label htmlFor="addr" >Address</label>
+      <input id="addr" type="text" />
+      <br />
+      <button type="submit" > Mint! </button>
+    </form>
   )
 }
+
+export default MintPage

b23be3ba6db72c9044c7481b9ba30c71cf8c65e7
cyruskarsan <cyruskarsan@gmail.com>
2021-11-17 22:42:00 -0800

    fix indentation

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import * as styles from '@src/components/mainApp/index.style'
 
 export default function Home() {
   return (
-      <MainApp>
-        <h1 className={styles.title}>NFT Marketplace</h1>
-        <p>put a button to enter app here</p>
-      </MainApp>
+    <MainApp>
+      <h1 className={styles.title}>NFT Marketplace</h1>
+      <p>put a button to enter app here</p>
+    </MainApp>
   )
 }

eb02e15990ff3c441a6d741d77f104253094a6e3
cyruskarsan <cyruskarsan@gmail.com>
2021-11-17 22:47:51 -0800

    correct indentation

--- components/walletConnectModal/WalletConnectModal.js
+++ components/walletConnectModal/WalletConnectModal.js
@@ -8,7 +8,7 @@ import { formatEther } from "@ethersproject/units";
 const WalletConnectModal = () => {
   const [open, setOpen] = useState(false);
 
-  const {activateBrowserWallet, account, chainId, error} = useEthers()
+  const { activateBrowserWallet, account, chainId, error } = useEthers()
   const etherBalance = useEtherBalance(account)
 
   const [chain, setChain] = useState('None')
@@ -25,30 +25,27 @@ const WalletConnectModal = () => {
     activateBrowserWallet()
   };
 
-  const connectWalletConnect = () => {}; // TODO
-  const connectCoinbaseWallet = () => {}; // TODO
-  const connectFormatic = () => {}; // TODO
-  const connectPortis = () => {}; // TODO
+  const connectWalletConnect = () => { }; // TODO
+  const connectCoinbaseWallet = () => { }; // TODO
+  const connectFormatic = () => { }; // TODO
+  const connectPortis = () => { }; // TODO
 
   return (
     <>
-      {!account  && <Button onClick={openModal} text="Connect Wallet" /> }
+      {!account && <Button onClick={openModal} text="Connect Wallet" />}
       {account && <h3> Account:
-            { account.slice(0, 6)}...{account.slice(
-              account.length - 4,
-              account.length
-            )} on {chain}</h3>}
+        {account.slice(0, 6)}...{account.slice(
+          account.length - 4,
+          account.length
+        )} on {chain}</h3>}
       <Modal label="Connect a Wallet" onClose={closeModal} open={open}>
         <div className={styles.connectHeading}>
           <h2>Connect a Wallet</h2>
-          <Button onClick={closeModal} text="Close Modal" />
+          <Button onClick={closeModal} text="Close" />
+
         </div>
         <div className={styles.connectGrid}>
           <Button onClick={connectMetaMask} text="MetaMask" />
-          <Button onClick={connectWalletConnect} text="WalletConnect" />
-          <Button onClick={connectCoinbaseWallet} text="Coinbase Wallet" />
-          <Button onClick={connectFormatic} text="Fortmatic" />
-          <Button onClick={connectPortis} text="Portis" />
         </div>
       </Modal>
     </>

83491a6fbcc3ab83d0dcf990e2d02119262772ee
DrewMcArthur <DrewMcArthur1@gmail.com>
2021-11-19 09:41:32 -0500

    add useEthers to view account

--- components/mintPage/MintPage.js
+++ components/mintPage/MintPage.js
@@ -1,11 +1,13 @@
 import mintNft from "@src/contractCalls/mintNft";
 import FileUploadModal from "./FileUploadModal";
+import { useEthers } from '@usedapp/core';
 // import MintNftRequest from "@src/contractCalls/MintNftRequest"
 import * as React from "react";
 // import * as ReactDOM from "react-dom"
 
 const MintPage = () =>
 {
+  const { account } = useEthers();
   const mint = ( event ) =>
   {
     // TODO: fix this to actually use the uploaded file, name, and wallet addr
@@ -13,6 +15,7 @@ const MintPage = () =>
     // const requestBody: MintNftRequest = {
     console.log( 'mint button clicked.' );
     console.log( event.target );
+    console.log( 'account: ', account );
     const requestBody = {
       name: event.target.name.value,
       file_url: event.target.file.value,
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import { MainApp } from '@src/components'
-import * as styles from '@src/components/mainApp/index.style'
+import { MainApp } from '@src/components';
+import * as styles from '@src/components/mainApp/index.style';
+import { useEthers } from '@usedapp/core';
 
-export default function Home() {
+export default function Home ()
+{
+  const { account } = useEthers();
+  console.log( "home page, getting account: ", account );
+  if ( account )
+  {
+    // redirect to dashboard
+    window.location = "/dashboard";
+  }
+
   return (
     <MainApp>
-      <h1 className={styles.title}>NFT Marketplace</h1>
-      <p>put a button to enter app here</p>
+      <h1 className={ styles.title }>Your Offchain Assets Dashboard</h1>
+      <p>SIWE</p>
     </MainApp>
-  )
+  );
 }

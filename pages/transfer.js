ebf353de21be5673395547c46616f85d16850583
DrewMcArthur <DrewMcArthur1@gmail.com>
2021-11-19 19:39:37 -0500

    create transfer coming soon page

---
+++ pages/transfer.js
@@ -0,0 +1,12 @@
+import { MainApp } from "@src/components";
+
+const Transfer = () =>
+{
+  return (
+    <MainApp>
+      <h1>Coming Soon!</h1>
+    </MainApp>
+  );
+};
+
+export default Transfer;

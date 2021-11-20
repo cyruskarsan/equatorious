c315f0f95735d7a0b1964b4ed925425f140db7db
DrewMcArthur <DrewMcArthur1@gmail.com>
2021-11-20 11:03:57 -0500

    semicolons

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,6 @@
-export { getFocusableChildNodes } from './nodes/getFocusableNodes'
-export { stringToId } from './string/string'
-export { mainNavLinks } from './nav/links'
-export { isPolygonNetwork } from './polygon/polygonUtil'
+export { getFocusableChildNodes } from './nodes/getFocusableNodes';
+export { stringToId } from './string/string';
+export { mainNavLinks } from './nav/links';
+export { isPolygonNetwork } from './polygon/polygonUtil';
 
-export const NFTPORT_API_KEY = "91e45e10-9125-46f3-b7f8-dfd8b547837f"
+export const NFTPORT_API_KEY = '91e45e10-9125-46f3-b7f8-dfd8b547837f';

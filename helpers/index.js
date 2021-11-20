5508ee4b12074702510f81452d75e80f9aec0206
DrewMcArthur <DrewMcArthur1@gmail.com>
2021-11-20 11:06:06 -0500

    nit spacing

--- helpers/index.js
+++ helpers/index.js
@@ -2,5 +2,4 @@ export { getFocusableChildNodes } from './nodes/getFocusableNodes';
 export { stringToId } from './string/string';
 export { mainNavLinks } from './nav/links';
 export { isPolygonNetwork } from './polygon/polygonUtil';
-
 export const NFTPORT_API_KEY = '91e45e10-9125-46f3-b7f8-dfd8b547837f';

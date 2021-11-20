7e0477effe781dcaf94a3d77e578eebd444ba8ca
DrewMcArthur <DrewMcArthur1@gmail.com>
2021-11-20 15:28:47 -0500

    configuration attempts

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  externals: {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom',
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+  },
 };
